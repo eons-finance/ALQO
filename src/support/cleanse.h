25a1e0a986e3e954b6e2524538e7a8ec3675ac02
Block Mechanic <blockmecha@gmail.com>
2020-07-25 11:02:26 +0200

    Even More renaming

--- src/support/cleanse.h
+++ src/support/cleanse.h
@@ -3,11 +3,11 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_SUPPORT_CLEANSE_H
-#define BITCOIN_SUPPORT_CLEANSE_H
+#ifndef ALQO_SUPPORT_CLEANSE_H
+#define ALQO_SUPPORT_CLEANSE_H
 
 #include <stdlib.h>
 
 void memory_cleanse(void *ptr, size_t len);
 
-#endif // BITCOIN_SUPPORT_CLEANSE_H
+#endif // ALQO_SUPPORT_CLEANSE_H
